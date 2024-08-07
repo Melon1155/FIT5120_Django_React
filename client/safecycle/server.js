e654caaabbbb3b32b0c226e968a2c8bda7ef4ec7
Melon1155 <tshe38@student.monash.edu>
2024-08-08 08:48:56 +1000

    Update server.js

--- client/safecycle/server.js
+++ client/safecycle/server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use the following lines to resolve the __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
